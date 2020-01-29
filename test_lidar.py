e4b5fa749ea525b9b64dfb6828c593104cf995f8
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2020-01-30 08:46:45 +0900

    最小に修正

--- test_lidar.py
+++ test_lidar.py
@@ -7,20 +7,31 @@ def test_lidar(cfg):
 
     V = dk.vehicle.Vehicle()
 
-    from donkeycar.parts.lidar import BreezySLAM, RPLidar, LidarPlot, MapToImage
+    from donkeycar.parts.lidar import BreezySLAM, LidarPlot, MapToImage
 
+    from donkeycar.parts.lidar import RPLidar
     rplidar = RPLidar()
     V.add(rplidar, outputs=['distances', 'angles'], threaded=True)
 
+    class PrintLidar(object):
+        def run(self, distances, angles):
+            print('[RPLidar] d:{} a:{}', str(distances), str(angles))
+        def shutdown(self):
+            pass
+    V.add(PrintLidar(), inputs=['distances', 'angles'])
+    '''
+    from donkeycar.parts.lidar import LidarPlot
     lidarplot = LidarPlot()
     V.add(lidarplot, inputs=['distances', 'angles'], outputs=['frame'])
 
+    from donkeycar.parts.lidar import MapToImage
     m2i = MapToImage()
     V.add(m2i, inputs=['frame'], outputs=['img_array'])
 
+    from donkeycar.parts.lidar import BreezySLAM
     slam = BreezySLAM()
     V.add(slam, inputs=['distances', 'angles', 'img_array'], outputs=['x', 'y', 'rad'])
-
+    '''
 
 
     try:
