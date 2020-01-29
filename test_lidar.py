fa880b535d0ad81ed76c5488c133ab960963ec35
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2020-01-29 16:35:43 +0900

    first commit

---
+++ test_lidar.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+import donkeycar as dk
+
+
+def test_lidar(cfg):
+
+    V = dk.vehicle.Vehicle()
+
+    from dk.parts.lidar import BreezySLAM, RPLidar, LidarPlot, MapToImage
+
+    rplidar = RPLidar()
+    V.add(rplidar, outputs=['distances', 'angles'], threaded=True)
+
+    lidarplot = LidarPlot()
+    V.add(lidarplot, inputs=['distances', 'angles'], outputs=['frame'])
+
+    slam = BreezySLAM()
+    V.add(slam, inputs=['distances', 'angles', 'frame'], outputs=['x', 'y', 'rad'])
+
+    m2i = MapToImage()
+    V.add(m2i, inputs=['frame'], outputs=['img_array'])
+
+    try:
+        V.start(rate_hz=cfg.DRIVE_LOOP_HZ, max_loop_count=cfg.MAX_LOOPS)
+    except KeyboardInterrupt:
+        print('exit')
+    finally:
+        pass
+
+if __name__ == '__main__':
+    cfg = dk.load_config()
+    test_lidar(cfg)
