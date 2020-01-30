e5ba8450e0ec363ff473f38ce92c74a6eff89b76
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2020-01-30 09:48:42 +0900

    frame可視化

--- test_lidar.py
+++ test_lidar.py
@@ -13,6 +13,7 @@ def test_lidar(cfg):
     rplidar = RPLidar()
     V.add(rplidar, outputs=['distances', 'angles'], threaded=True)
 
+    '''
     class PrintLidar(object):
         def run(self, distances, angles):
             print('[RPLidar] d:{} a:{}'.format(str(distances), str(angles)))
@@ -21,10 +22,19 @@ def test_lidar(cfg):
             pass
     V.add(PrintLidar(), inputs=['distances', 'angles'])
     '''
+
     from donkeycar.parts.lidar import LidarPlot
     lidarplot = LidarPlot()
     V.add(lidarplot, inputs=['distances', 'angles'], outputs=['frame'])
 
+    class PrintLidar(object):
+        def run(self, frame):
+            print('[LidarPlot] frame:{}'.format(str(type(frame))))
+            print('[RPLidar] frame:{} a:{}'.format(str(frame)))
+        def shutdown(self):
+            pass
+    V.add(PrintLidar(), inputs=['frame'])
+    '''
     from donkeycar.parts.lidar import MapToImage
     m2i = MapToImage()
     V.add(m2i, inputs=['frame'], outputs=['img_array'])
