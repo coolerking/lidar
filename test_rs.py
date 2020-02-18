09ac2b48487311fa8f868aeb03015fafaa995d7b
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2020-02-18 16:15:36 +0900

    ドキュメント追加、独自パーツ作成

--- test_rs.py
+++ test_rs.py
@@ -6,7 +6,9 @@ import donkeycar as dk
 
 
 def test_rs(cfg):
-
+    """
+    Donekycar v3.1.1 上のパーツクラスを使用。
+    """
     V = dk.vehicle.Vehicle()
 
     from donkeycar.parts.realsense2 import RS_T265
@@ -76,8 +78,55 @@ def test_rs(cfg):
     except KeyboardInterrupt:
         print('exit')
     finally:
-        pass
+        print('done')
+
+def test_rs2(cfg):
+    """
+    独自パーツを使用。
+    """
+    print('Use original T265 part class')
+    V = dk.vehicle.Vehicle()
+    V.mem['enc_vel_ms'] = 0.0
+    from realsense import RealSenseT265
+    V.add(RealSenseT265(image_output=True, debug=True),
+        inputs=['enc_vel_ms'],
+        outputs=['pos_x', 'pos_y', 'pos_z',
+            'vel_x', 'vel_y', 'vel_z',
+            'acc_x', 'acc_y', 'acc_z',
+            'roll', 'pitch', 'yaw',
+            'image_array'],
+        threaded=False)
+
+
+    inputs=['image_array',
+            'pos_x', 'pos_y', 'pos_z',
+            'vel_x', 'vel_y', 'vel_z',
+            'acc_x', 'acc_y', 'acc_z',
+            'roll', 'pitch', 'yaw',
+        ]
+
+    types=['image_array',
+           'float', 'float', 'float',
+           'float', 'float', 'float',
+           'float', 'float', 'float',
+           'float', 'float', 'float',
+        ]
+
+    from donkeycar.parts.datastore import TubHandler
+    th = TubHandler(path=cfg.DATA_PATH)
+    tub = th.new_tub_writer(inputs=inputs, types=types, user_meta=[])
+    V.add(tub, inputs=inputs, outputs=["tub/num_records"])
+
+    try:
+        V.start(rate_hz=cfg.DRIVE_LOOP_HZ,
+            max_loop_count=cfg.DRIVE_LOOP_HZ * 60) # 1min
+            ## max_loop_count=cfg.MAX_LOOPS) # infinity
+    except KeyboardInterrupt:
+        print('exit')
+    finally:
+        print('done')
 
 if __name__ == '__main__':
     cfg = dk.load_config()
-    test_rs(cfg)
+    #test_rs(cfg)
+    test_rs2(cfg)
